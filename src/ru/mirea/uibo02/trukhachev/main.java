0ca2965d4346c6a64f5bcc100675ea3fd74e4e98
Ivan Trukhachev <mrstrikeinfo@gmail.com>
2023-09-10 17:48:02 +0300

    Task 6

--- src/ru/mirea/uibo02/trukhachev/main.java
+++ src/ru/mirea/uibo02/trukhachev/main.java
@@ -1,7 +1,18 @@
 package ru.mirea.uibo02.trukhachev;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class main {
     public static void main(String[] args)
+    {
+        tsk3(args);
+        tsk4(args);
+        tsk5(args);
+        tsk6(args);
+    }
+
+    public static void tsk3(String[] args)
     {
         int[] data = {0, 1, 2, 3, 4, 5};
         int sum = 0;
@@ -31,8 +42,43 @@ public class main {
         }
         while
         (
-             b < data2.length
+                b < data2.length
         );
         System.out.println("summa do while = " + sum2);
+    }
+
+    public static void tsk4(String[] args)
+    {
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+
+    public static void tsk5(String[] args)
+    {
+        int n = 10;
+        double sum = 0.0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += 1.0 / i;
+            System.out.printf("%.2f ", sum);
+        }
+        System.out.println();
+    }
+
+    public static void tsk6(String[] args)
+    {
+        int[] array = new int[10];
+        for(int i = 0; i < array.length; i++)
+        {
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println("Generated array: ");
+        System.out.println(Arrays.toString(array));
+        Arrays.sort(array);
+        System.out.println("Sorted by math: ");
+        System.out.println(Arrays.toString(array));
     }
 }
